56b7daf18bc8a340f6e17fbff839a354ae1f284e
Bita De La Rosa <bita@bitas-air.lan>
2021-06-10 16:11:06 -0700

    operators

--- script.js
+++ script.js
@@ -1,81 +1,109 @@
-// // Values
+ /*
+// Values
 
-// console.log("Bita");
-// console.log(23);
+console.log("Bita");
+console.log(23);
 
-// let firstName = "Bita"; 
-// console.log(firstName);
+let firstName = "Bita"; 
+console.log(firstName);
 
-// let myFirstJob = 'Programmer';
-// let myCurrentJob = 'Teacher';
-
-
-// //* Practice assignment 1 LECTURE: Values and Variables
-// //1. Declare variables called 'country', 'continent' and 'population' and
-// //assign their values according to your own country (population in millions)
-// //2. Log their values to the console
-
-// let country = "United States";
-// let continent = "North America";
-// let population = 10;
-
-// console.log (country, continent, population);
-// //------------------------------------------------------------------------------------------------------------
-// // Data Types
-
-// let javascriptIsFun = true;
-// console.log(typeof javascriptIsFun);
-// console.log(typeof 30);
-// console.log(typeof "Bita");
-
-// javascriptIsFun = "YES!";
-// console.log(typeof javascriptIsFun);
-
-// let year;
-// console.log(year);
-// year = 2021;
-// console.log(typeof year);
+let myFirstJob = 'Programmer';
+let myCurrentJob = 'Teacher';
 
 
-// /* Practice assignment 2 LECTURE: Data Types
-// 1. Declare a variable called 'isIsland' and set its value according to your
-// country. The variable should hold a Boolean value. Also declare a variable
-// 'language', but don't assign it any value yet
-// 2. Log the types of 'isIsland', 'population', 'country' and 'language'
-// to the console */
+// Practice assignment 1 LECTURE: Values and Variables
+//1. Declare variables called 'country', 'continent' and 'population' and
+//assign their values according to your own country (population in millions)
+//2. Log their values to the console
 
-// let isIsland = false;
-// let language;
-// console.log(typeof isIsland);
-// console.log(typeof population);
-// console.log(typeof country);
-// console.log(typeof language);
+let country = "United States";
+let continent = "North America";
+let population = 10;
 
-// //------------------------------------------------------------------------------------------------------------
-// // Let, Const, Var
+console.log (country, continent, population);
+//------------------------------------------------------------------------------------------------------------
+// Data Types
 
-// let age = 31;
-// age = 31;
+let javascriptIsFun = true;
+console.log(typeof javascriptIsFun);
+console.log(typeof 30);
+console.log(typeof "Bita");
 
-// const birthYear = 1990;
+javascriptIsFun = "YES!";
+console.log(typeof javascriptIsFun);
+
+let year;
+console.log(year);
+year = 2021;
+console.log(typeof year);
 
 
-/* Practice assignment 3 LECTURE: let, const and var
+// Practice assignment 2 LECTURE: Data Types
+1. Declare a variable called 'isIsland' and set its value according to your
+country. The variable should hold a Boolean value. Also declare a variable
+'language', but don't assign it any value yet
+2. Log the types of 'isIsland', 'population', 'country' and 'language'
+to the console 
+
+let isIsland = false;
+let language;
+console.log(typeof isIsland);
+console.log(typeof population);
+console.log(typeof country);
+console.log(typeof language);
+
+//------------------------------------------------------------------------------------------------------------
+// Let, Const, Var
+
+let age = 31;
+age = 31;
+
+const birthYear = 1990;
+
+
+// Practice assignment 3 LECTURE: let, const and var
 1. Set the value of 'language' to the language spoken where you live (some
 countries have multiple languages, but just choose one)
 2. Think about which variables should be const variables (which values will never
 change, and which might change?). Then, change these variables to const.
-3. Try to change one of the changed variables now, and observe what happens */
+3. Try to change one of the changed variables now, and observe what happens 
 
 const continent = "North America";
 const country = "United States"
 const isIsland = false;
 let language = "English";
-let population = 10;
 console.log (continent, country, isIsland, population, language);
+
+ 
 
 //------------------------------------------------------------------------------------------------------------
 // Basic Operators
+
+const now = 2020
+const ageBita = now - 1990;
+const ageChris = now - 1988;
+console.log(ageBita, ageChris);
+console.log(ageBita * 2, ageChris / 2);
+
+const firstName = "Bita";
+const lastName = "De La Rosa";
+console.log(firstName + " " + lastName);
+
+// Assignment operators
+let x = 10 + 5; // 15
+ x += 10; // x = x + 10 = 25
+ x *= 4; // x = x * 4 = 100
+ x++; // x = x + 1 = 101
+ x--; // x = x - 1 = 100
+console.log(x);
+
+// Comparison operators
+console.log(ageBita > ageChris);
+console.log(ageBita < ageChris);
+console.log(ageBita >= 20);
+console.log(ageBita >= 40);
+
+*/
 
 /* Practice assignment 4 LECTURE: Basic Operators
 1. If your country split in half, and each half would contain half the population,
