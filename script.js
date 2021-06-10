1066dbf6dbef6b2b64d098f5c57251aa0fadd5f8
Bita De La Rosa <bita@bitas-air.lan>
2021-06-10 13:33:08 -0700

    variables

--- script.js
+++ script.js
@@ -1,2 +1,23 @@
 let js = "amazing";
-      if (js = "amazing") alert("javascript is FUN!");
+
+// Values
+
+console.log("Bita");
+console.log(23);
+
+let firstName = "Bita"; 
+console.log(firstName);
+
+
+
+      //Practice assignment 1
+      //LECTURE: Values and Variables
+//1. Declare variables called 'country', 'continent' and 'population' and
+//assign their values according to your own country (population in millions)
+//2. Log their values to the console
+
+let country = "United States";
+let continent = "North America";
+let population = 10000000;
+
+console.log (country, continent, population);
