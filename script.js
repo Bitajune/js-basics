900662331216ce88e498ed624ec6fb72a0ea2975
Bitajune <beheshtibita@gmail.com>
2021-06-14 13:50:11 -0700

    switch statement

--- script.js
+++ script.js
@@ -397,7 +397,7 @@ if (hasDriversLicense && hasGoodVision && !isTired) {
   console.log("Someone else should drive");
 }
 
-*/
+
 
 //CODING CHALLENGE #3
 // const dolphinsAverage = (96 + 108 + 89) / 3;
@@ -431,6 +431,56 @@ if (dolphinsAverage > koalasAverage && dolphinsAverage >= 100) {
   console.log("No one wins!");
 }
 
+
 // The switch statement
+const day = "wednesday";
+
+switch (day) {
+  case "monday": //day === 'monday'
+    console.log("Plan course structure");
+    console.log("Go to coding meetup");
+    break;
+  case "tuesday":
+    console.log("Prepare theory videos");
+    break;
+  case "wednesday":
+  case "thursday":
+    console.log("Write code examples");
+    break;
+  case "friday":
+    console.log("Record videos");
+    break;
+  case "satruday":
+  case "sunday":
+    console.log("Relax");
+    break;
+  default:
+    console.log("Not a valid day");
+}
+practice
+
+const language = "farsi";
+
+switch (language) {
+  case "chinese":
+  case "mandarin":
+    console.log("Most number of natice speakers");
+    break;
+  case "spanish":
+    console.log("2nd place in number of native speakers");
+    break;
+  case "english":
+    console.log("3rd place");
+    break;
+  case "hindi":
+    console.log("Number 4");
+    break;
+  case "arabic":
+    console.log("5th most spoken language");
+    break;
+  default:
+    console.log("That's a great language!");
+}
+*/
 
 // The Conditional operator
